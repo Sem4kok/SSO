c66f5294d2ef4a12c71751241e47759d1460c306
Sem4kok <w1usis@mail.ru>
2024-04-27 15:44:11 +0300

    feat(handlers): handlers added

--- internal/grpc/auth/server.go
+++ internal/grpc/auth/server.go
@@ -8,29 +8,120 @@ import (
 	"google.golang.org/grpc/status"
 )
 
-type serverAPI struct {
-	ssov1.UnimplementedAuthServer
+type Auth interface {
+	Login(ctx context.Context,
+		email string,
+		password string,
+		appID int32,
+	) (token string, err error)
+	RegisterUser(ctx context.Context,
+		email string,
+		password string,
+	) (userID int64, err error)
+	IsAdmin(ctx context.Context, userID int64) (isAdmin bool, err error)
 }
 
-func Register(server *grpc.Server) {
-	ssov1.RegisterAuthServer(server, &serverAPI{})
+const (
+	emptyValue = 0
+)
+
+type serverAPI struct {
+	ssov1.UnimplementedAuthServer
+	auth Auth
+}
+
+func Register(server *grpc.Server, auth Auth) {
+	ssov1.RegisterAuthServer(server, &serverAPI{auth: auth})
 }
 
 func (s *serverAPI) Register(
 	ctx context.Context,
 	req *ssov1.RegisterRequest,
 ) (*ssov1.RegisterResponse, error) {
+	if err := validateRegister(req); err != nil {
+		return nil, err
+	}
+
+	userID, err := s.auth.RegisterUser(ctx, req.GetEmail(), req.GetPassword())
+	if err != nil {
+
+		// TODO implement
+		return nil, status.Error(codes.Internal, "internal msg")
+	}
+
 	return nil, status.Errorf(codes.Unimplemented, "method Register not implemented")
 }
 
-func (s *serverAPI) Login(ctx context.Context,
+func (s *serverAPI) Login(
+	ctx context.Context,
 	req *ssov1.LoginRequest,
 ) (*ssov1.LoginResponse, error) {
+	if err := validateLogin(req); err != nil {
+		return nil, err
+	}
+
+	// if validation pass successful
+	userID, err := s.auth.Login(ctx, req.GetEmail(), req.GetPassword(), req.GetAppId())
+	if err != nil {
+
+		// TODO implement
+		return nil, status.Error(codes.Internal, "internal msg")
+	}
+
 	return nil, status.Errorf(codes.Unimplemented, "method Login not implemented")
 }
 
 func (s *serverAPI) IsAdmin(ctx context.Context,
 	req *ssov1.IsAdminRequest,
 ) (*ssov1.IsAdminResponse, error) {
+	if err := validateIsAdmin(req); err != nil {
+		return nil, err
+	}
+
+	isAdmin, err := s.auth.IsAdmin(ctx, req.GetUserId())
+	if err != nil {
+
+		// TODO implement
+		return nil, status.Error(codes.Internal, "internal msg")
+	}
+
 	return nil, status.Errorf(codes.Unimplemented, "method IsAdmin not implemented")
+}
+
+// validateLogin checks for user login input correctly
+func validateLogin(req *ssov1.LoginRequest) error {
+	if req.GetEmail() == "" {
+		return status.Error(codes.InvalidArgument, "email is required")
+	}
+
+	if req.GetPassword() == "" {
+		return status.Error(codes.InvalidArgument, "password is required")
+	}
+
+	if req.GetAppId() == emptyValue {
+		return status.Error(codes.InvalidArgument, "app id is required")
+	}
+
+	return nil
+}
+
+// validateRegister checks for user register input correctly
+func validateRegister(req *ssov1.RegisterRequest) error {
+	if req.GetEmail() == "" {
+		return status.Error(codes.InvalidArgument, "email is required")
+	}
+
+	if req.GetPassword() == "" {
+		return status.Error(codes.InvalidArgument, "email is required")
+	}
+
+	return nil
+}
+
+func validateIsAdmin(req *ssov1.IsAdminRequest) error {
+	if req.GetUserId() == emptyValue {
+		return status.Error(codes.InvalidArgument, "email is required")
+	}
+
+	return nil
 }
