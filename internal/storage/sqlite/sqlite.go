4ec832d088c84105c9a3a7c38cbd560c2e38b38b
Sem4kok <w1usis@mail.ru>
2024-04-29 10:09:31 +0300

    feat(storage): connection method added

---
+++ internal/storage/sqlite/sqlite.go
@@ -0,0 +1,23 @@
+package sqlite
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type Storage struct {
+	db *sql.DB
+}
+
+// Connect connects to db, returns Storage struct
+func Connect(storagePath string) (*Storage, error) {
+	const op = "storage.sqlite.Connect"
+
+	db, err := sql.Open("sqlite3", storagePath)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return &Storage{db: db}, nil
+}
