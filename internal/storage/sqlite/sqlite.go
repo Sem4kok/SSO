67911302f1f1ab4fd17d6e3f837c5b64a9105506
Sem4kok <w1usis@mail.ru>
2024-04-29 10:33:19 +0300

    feat(storage): SaveUser method implemented

--- internal/storage/sqlite/sqlite.go
+++ internal/storage/sqlite/sqlite.go
@@ -1,9 +1,17 @@
 package sqlite
 
 import (
+	"SSO/internal/storage"
+	"context"
 	"database/sql"
+	"errors"
 	"fmt"
+	"github.com/mattn/go-sqlite3"
 	_ "github.com/mattn/go-sqlite3"
+)
+
+const (
+	emptyValue = 0
 )
 
 type Storage struct {
@@ -12,7 +20,7 @@ type Storage struct {
 
 // Connect connects to db, returns Storage struct
 func Connect(storagePath string) (*Storage, error) {
-	const op = "storage.sqlite.Connect"
+	const op = "sqlite.Connect"
 
 	db, err := sql.Open("sqlite3", storagePath)
 	if err != nil {
@@ -20,4 +28,32 @@ func Connect(storagePath string) (*Storage, error) {
 	}
 
 	return &Storage{db: db}, nil
+}
+
+func (s *Storage) SaveUser(ctx context.Context, email string, passHash []byte) (int64, error) {
+	const op = "sqlite.SaveUser"
+
+	// Prepare instead of exec because of better performance first one
+	stmt, err := s.db.Prepare("INSERT INTO users (email, pass_hash) VALUES (?, ?)")
+	if err != nil {
+		return emptyValue, fmt.Errorf("%s : %w", op, err)
+	}
+
+	res, err := stmt.ExecContext(ctx, email, passHash)
+	if err != nil {
+		var sqliteError sqlite3.Error
+
+		if errors.As(err, &sqliteError) && errors.Is(sqliteError, sqlite3.ErrConstraintUnique) {
+			return emptyValue, fmt.Errorf("%s, %w", op, storage.ErrUserAlreadyExists)
+		}
+
+		return emptyValue, fmt.Errorf("%s : %w", op, err)
+	}
+
+	newID, err := res.LastInsertId()
+	if err != nil {
+		return emptyValue, fmt.Errorf("%s : %w", op, err)
+	}
+
+	return newID, nil
 }
