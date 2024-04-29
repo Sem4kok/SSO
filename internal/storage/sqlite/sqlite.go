db7905362c8ef6582b1d74f12787423638eecf82
Sem4kok <w1usis@mail.ru>
2024-04-29 10:56:04 +0300

    feat(storage): get user method added

--- internal/storage/sqlite/sqlite.go
+++ internal/storage/sqlite/sqlite.go
@@ -1,6 +1,7 @@
 package sqlite
 
 import (
+	"SSO/internal/domain/models"
 	"SSO/internal/storage"
 	"context"
 	"database/sql"
@@ -12,6 +13,10 @@ import (
 
 const (
 	emptyValue = 0
+)
+
+var (
+	emptyUser = &models.User{}
 )
 
 type Storage struct {
@@ -30,6 +35,8 @@ func Connect(storagePath string) (*Storage, error) {
 	return &Storage{db: db}, nil
 }
 
+// SaveUser method of Storage.
+// Implements saving user into database sqlite
 func (s *Storage) SaveUser(ctx context.Context, email string, passHash []byte) (int64, error) {
 	const op = "sqlite.SaveUser"
 
@@ -56,4 +63,30 @@ func (s *Storage) SaveUser(ctx context.Context, email string, passHash []byte) (
 	}
 
 	return newID, nil
+}
+
+// User method of Storage.
+// Implements getting user from database
+func (s *Storage) User(ctx context.Context, email string) (user *models.User, err error) {
+	const op = "sqlite.User"
+
+	// prepare for exec
+	stmt, err := s.db.Prepare("SELECT * FROM users WHERE (email) VALUES (?)")
+	if err != nil {
+		return emptyUser, fmt.Errorf("%s : %w", op, err)
+	}
+
+	// exec statement context
+	row := stmt.QueryRowContext(ctx, email)
+
+	user = &models.User{}
+	if err := row.Scan(&user.Email, &user.ID, &user.PassHash); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return emptyUser, fmt.Errorf("%s : %w", op, storage.ErrUserNotFound)
+		}
+
+		return emptyUser, fmt.Errorf("%s : %w", op, err)
+	}
+
+	return user, nil
 }
